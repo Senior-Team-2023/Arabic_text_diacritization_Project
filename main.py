4656830e6248ca8d8d0ce2ed8dfd0182d3a71fef
EngPeterAtef <peter_atef@stud.cu.edu.eg>
2023-11-27 11:42:13 +0200

    update git ignore and add draft tokenizer files

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import utils
 
 # Read data from train.txt and filter it from unwanted patterns
-training_set = utils.read_data('train')
-
+training_set = utils.read_data("train")
+print(training_set[0:100])
+tokenized_training_set = utils.char_tokenizer(training_set)
+print(tokenized_training_set[0:10])
 # Extract Features from training set
 
 # Build Model
--- utils.py
+++ utils.py
@@ -1,13 +1,26 @@
 import re
 import numpy as np
+import tkseem as tk
+
+# from nltk.tokenize.stanford_segmenter import StanfordSegmenter
+
 
 # read ./dataset/{file_name}.txt line by line and return a list of filtered strings
 def read_data(file_name):
-    with open(f'./dataset/{file_name}.txt', 'r', encoding='utf-8') as f:
-        train_set = f.read().splitlines()
-        return filter_data(train_set)
-    
+    with open(f"./dataset/{file_name}.txt", "r", encoding="utf-8") as f:
+        # train_set = f.read().splitlines()
+        return filter_data(f.read())
+
 
 # filter data takes a list of strings and removes this patern ( number / number )
 def filter_data(data):
-    return [re.sub(r'\( \d+ / \d+ \)', '', line) for line in data]
+    # return [re.sub(r'\( \d+ (/ \d+)? \)', '', line) for line in data]
+    # regex to remove all special characters
+    data = re.sub(r"\( \d+ (/ \d+)? \)", "", data)
+    data = re.sub(r"[^\w\s]", "", data)
+    return data
+
+
+# character tokenizer
+def char_tokenizer(data):
+    return [list(line) for line in data]
