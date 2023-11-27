4869f8a19b2880a8805c63f0f7f6d628ff85fb44
EngPeterAtef <peter_atef@stud.cu.edu.eg>
2023-11-27 14:04:13 +0200

    data cleaning draft 1

--- utils.py
+++ utils.py
@@ -8,21 +8,25 @@ from character_tokenizer import CharacterTokenizer
 def read_data(file_name):
     with open(f"./dataset/{file_name}.txt", "r", encoding="utf-8") as f:
         # train_set = f.read().splitlines()
-        return filter_data(f.read())
+        return f.read()
 
 
-# filter data takes a list of strings and removes this patern ( number / number )
+# filter data takes a list of strings and removes unwanted patterns
 def filter_data(data):
-    # return [re.sub(r'\( \d+ (/ \d+)? \)', '', line) for line in data]
+    # data = re.sub(r"\( \d+ (/ \d+)? \)", "", data)
+    # remove all numbers
+    data = re.sub(r"\d+", "", data)
     # regex to remove all special characters
-    data = re.sub(r"\( \d+ (/ \d+)? \)", "", data)
-    data = re.sub(r"[^\w\s]", "", data)
+    data = re.sub(r"[][//,;?()$:-{}_]", "", data)
+    # remove all english letters
+    data = re.sub(r"[a-zA-Z]", "", data)
+    # Substituting multiple spaces with single space    
+    data = re.sub(r'([ \r\t\f])+', ' ', data, flags=re.I)
     return data
 
 
 # character tokenizer
 def char_tokenizer(data):
-    # return [list(line) for line in data]
     tokenizer = CharacterTokenizer()
     tokenizer.train("./dataset/train.txt")
     return tokenizer.tokenize(data[0:1000])
