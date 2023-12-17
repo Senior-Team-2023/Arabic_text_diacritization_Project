69cf4f6354b8df0ddc2fb0a9c44c30a809fcc526
Bemoi Erian <bemoi.erian@gmail.com>
2023-12-17 20:26:55 +0200

    edits in pytorch rnn

--- Models/rnn_pytorch.py
+++ Models/rnn_pytorch.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 # Define the RNN model
 class RNN_Model(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, num_layers=1):
@@ -8,13 +9,16 @@ class RNN_Model(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         # self.output_size = output_size
-        self.rnn = nn.RNN(input_size, hidden_size, num_layers,batch_first=True)
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        # self.rnn = nn.RNN(input_size, hidden_size, num_layers)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)  # Initial hidden state
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(
+            x.device
+        )  # Initial hidden state
         out, _ = self.rnn(x, h0)  # RNN output and last hidden state
-        out = self.fc(out[:, -1, :])  # Pass the last time-step output of RNN to a Fully connected layer
+        out = self.fc(
+            out[:, -1, :]
+        )  # Pass the last time-step output of RNN to a Fully connected layer
         return out
-
-
